c87d6aa53d07060e8946c659c9d95f0344ca6f75
Joshua Lewis <joshualewis@adminisatorsmbp.home>
2020-10-30 12:24:50 -0400

    Completed challenge 3

--- JS Medium/script.js
+++ JS Medium/script.js
@@ -1,1 +1,10 @@
-var userTone = prompt(``)
+
+var userTone = prompt(`Hello`);
+
+if (userTone === userTone.toUpperCase()) {
+  console.log(`You are SHOUTING`);
+} else if (userTone === userTone.toLowerCase()) {
+  console.log(`You are whispering`);
+} else {
+  console.log(`You are niether SHOUTING or whispering`);
+}
